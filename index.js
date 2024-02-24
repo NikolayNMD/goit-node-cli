791ff81d3fe2c56718001012da6449cbb53b5f51
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2024-02-24 23:40:41 +0200

    done all cases

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { program } from "commander";
+import * as services from "./contacts.js";
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,22 +12,31 @@ program.parse();
 
 const options = program.opts();
 
-async function invokeAction({ action, id, name, email, phone }) {
+async function invokeAction({ action, id, ...data }) {
   switch (action) {
     case "list":
-      // ...
+      const contacts = await services.listContacts();
+      console.table(contacts);
       break;
 
     case "get":
-      // ... id
+      const contact = await services.getContactById(id);
+      console.log(contact);
       break;
 
     case "add":
-      // ... name email phone
+      const newContact = await services.addContact(data);
+      console.log(newContact);
       break;
 
     case "remove":
-      // ... id
+      const removedContact = await services.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    case "update":
+      const updatedContact = await services.updateContactById(id, data);
+      console.log(updatedContact);
       break;
 
     default:
